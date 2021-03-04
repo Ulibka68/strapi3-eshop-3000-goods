680e9b049d68792341466e8527b18445eee0d929
Ulibka68 <gena68@inbox.ru>
2021-03-04 17:07:58 +0300

    залил все файлы

---
+++ plugins/point-list/admin/src/index.js
@@ -0,0 +1,57 @@
+import pluginPkg from '../../package.json';
+import pluginId from './pluginId';
+import App from './containers/App';
+import Initializer from './containers/Initializer';
+import lifecycles from './lifecycles';
+import trads from './translations';
+
+export default strapi => {
+  const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+  const icon = pluginPkg.strapi.icon;
+  const name = pluginPkg.strapi.name;
+
+  const plugin = {
+    blockerComponent: null,
+    blockerComponentProps: {},
+    description: pluginDescription,
+    icon,
+    id: pluginId,
+    initializer: Initializer,
+    injectedComponents: [],
+    isReady: false,
+    isRequired: pluginPkg.strapi.required || false,
+    layout: null,
+    lifecycles,
+    mainComponent: App,
+    name,
+    preventComponentRendering: false,
+    trads,
+    menu: {
+      pluginsSectionLinks: [
+        {
+          destination: `/plugins/${pluginId}`,
+          icon,
+          label: {
+            id: `${pluginId}.plugin.name`,
+            defaultMessage: name,
+          },
+          name,
+          // If the plugin has some permissions on whether or not it should be accessible
+          // depending on the logged in user's role you can set them here.
+          // Each permission object performs an OR comparison so if one matches the user's ones
+          // the link will be displayed
+          // permissions: [{ action: 'plugins::content-type-builder.read', subject: null }],
+          permissions: [
+            // Uncomment to set the permissions of the plugin here
+            // {
+            //   action: '', // the action name should be plugins::plugin-name.actionType
+            //   subject: null,
+            // },
+          ],
+        },
+      ],
+    },
+  };
+
+  return strapi.registerPlugin(plugin);
+};
